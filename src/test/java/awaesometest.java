952e5c868a06db4a41fd98a37c54b8859194747e
Rajeev Dahal <rajeev88@gmail.com>
2020-10-06 20:59:10 +0100

    add more text

--- src/test/java/awaesometest.java
+++ src/test/java/awaesometest.java
@@ -4,4 +4,10 @@ public class awaesometest {
     //green mean it is under staging are
     //grey in. idea folder means its ignored
     //wehn the color is normal , it means its commited and changes
+
+
+   // i just pulled the new change from github that i did not have locally now my project
+           // is in sync with remote repository,
+    // now i made more awesome changes and ai am about to push this
+    //back to gitHub
 }
