e261254179f17623011f28b27915f6464c85b104
Rajeev Dahal <rajeev88@gmail.com>
2020-10-06 20:46:13 +0100

    added new file for awesome feature

---
+++ src/test/java/awaesometest.java
@@ -0,0 +1,7 @@
+public class awaesometest {
+
+    //read means untracked
+    //green mean it is under staging are
+    //grey in. idea folder means its ignored
+    //wehn the color is normal , it means its commited and changes
+}
